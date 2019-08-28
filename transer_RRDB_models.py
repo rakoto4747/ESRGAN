d1f6246115933f7a74a8da6e35c34d976c7e90ed
rakoto4747 <46997755+rakoto4747@users.noreply.github.com>
2019-08-28 08:35:12 +0300

    Update transer_RRDB_models.py

--- transer_RRDB_models.py
+++ transer_RRDB_models.py
@@ -7,7 +7,7 @@ save_path = '/content/gdrive/My Drive/ESRGAN/ESRGAN-test/models/ESRGANplus_x4_ne
 
 crt_model = arch.RRDBNet(3, 3, 64, 23, gc=32)
 crt_net = crt_model.state_dict()
-print(crt_net)
+print(crt_net.keys())
 
 load_net_clean = {}
 for k, v in pretrained_net.items():
@@ -16,7 +16,7 @@ for k, v in pretrained_net.items():
     else:
         load_net_clean[k] = v
 pretrained_net = load_net_clean
-print(pretrained_net)
+print(pretrained_net.keys())
 
 print('###################################\n')
 tbd = []
