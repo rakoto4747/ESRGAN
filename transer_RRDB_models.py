7f3ed94845f34ceeb9fb0cdb42aae22927375a0a
rakoto4747 <46997755+rakoto4747@users.noreply.github.com>
2019-10-07 16:36:46 +0300

    Update transer_RRDB_models.py

--- transer_RRDB_models.py
+++ transer_RRDB_models.py
@@ -5,6 +5,8 @@ import RRDBNet_arch as arch
 pretrained_net = torch.load('/content/gdrive/My Drive/latest_D.pth')
 save_path = '/content/gdrive/My Drive/latest_D_new_arch.pth'
 
+print(pretrained_net)
+
 crt_model = arch.RRDBNet(3, 3, 64, 23, gc=32)
 crt_net = crt_model.state_dict()
 
